411aa7f5cb456532dee6c6532c7e3970e0524072
Ares-Dji <ares.li@dji-innovation.com>
2017-04-22 18:42:38 +0800

    Add projects

---
+++ SouJiao/SouJiao/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SouJiao
+//
+//  Created by Ares on 2017/4/22.
+//  Copyright © 2017年 Ares. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
